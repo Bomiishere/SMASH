a1008b4fbe7df73033d440e444a0adb53c41f0cc
Bomi <bomichen@skbank.com.tw>
2018-11-13 21:59:49 +0800

    init project

---
+++ SMASH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SMASH
+//
+//  Created by Bomi on 2018/11/13.
+//  Copyright © 2018年 Bomi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
